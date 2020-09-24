2135f519dba7c9014e2d95287c2ae34da28649aa
YesunHuang <tgbus944812203@hotmail.com>
2020-09-24 23:59:13 +0800

    complete!

--- 01/main.c
+++ 01/main.c
@@ -3,15 +3,18 @@
 void main() {
 	//输出计算用
 	int seed = Seed();
+
 	/* debug测试块1
 	printf("seed = %d \n",seed);
 	int firstRandNum = Shrage(seed);
 	printf("First random number = %d \n", firstRandNum);*/
 
 	int N,i=0;
-	double range[2], * randOut;
+	double range[2] = { 0,1 };
+
+	//double* randOut;
 	//N,start,end
-	scanf("%d %lf %lf", &N, &range[0], &range[1]);
+	//scanf("%d %lf %lf", &N, &range[0], &range[1]);
 	/* debug测试块2
 	randOut = RandN(seed, range, N);
 	for ( i = 0; i < N; i++)
@@ -19,7 +22,7 @@ void main() {
 		printf("%.9e ", randOut[i]);
 	}
 	printf("\n");*/
-	char name[30];
+	//char name[30];
 	/*plot 二维点产生块，debug测试块3
 	scanf("%s", &name);
 	RandNToDat(seed, range, N, name);
@@ -29,8 +32,12 @@ void main() {
 	scanf("%d", &l);
 	printf("%.9e", ReadAndCalCorr(name, l, N));*/
 	
+	//二维点数据
+	RandNToDat(seed, range, 10000, "data_plot.dat");
+
+
+	//测试独立性与偏差
 	int l[5] = { 1,6,10,15,20 }, k[5] = { 1, 2, 3, 4, 5 }, * p;
-	char i_c;
 	FILE* fp;
 	if ((fp = fopen("check.dat", "w")) == NULL)
 	{
@@ -48,25 +55,23 @@ void main() {
 	}
 	fprintf(fp, "\n");
 	//自动测试多个范围
-	for ( i = 1; i < 5; i++)
+	for ( i = 1; i < 9; i++)
 	{
-		//自动生成文件 ps.看,这就是C，真的烂
-		i_c = (char)(i + 48);
-		strcpy(name, "data_N=");
-		name[7] = i_c; name[8] = 0;
-		strcat(name, ".dat");
-		//自动生成数据
 		N = (int)pow(10, i);
-		RandNToDat(seed, range,N, name);
+		RandNToDat(seed, range,N, "data.dat");
 		fprintf(fp,"%d\t",i);
 		for (p = l; p <= &l[4]; p++)
 		{
-			fprintf(fp, "%.9e\t", ReadAndCalCorr(name, *p, N));
+			fprintf(fp, "%.9e\t", ReadAndCalCorr("data.dat", *p, N));
 		}
 		for (p = k; p <= &k[4]; p++)
 		{
-			fprintf(fp, "%.9e\t", ReadAndCalAvg(name, *p, N));
+			fprintf(fp, "%.9e\t", ReadAndCalAvg("data.dat", *p, N));
 		}
 		fprintf(fp, "\n");
+		printf("complete:%d/8\n", i);
 	}
+	//记录种子
+	fprintf(fp,"seed=%d\n", seed);
+	fclose(fp);
 }
