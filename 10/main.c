dce1131923a681e5f64852f4759c1cdac2e03dfa
YesunHuang <tgbus944812203@hotmail.com>
2020-10-21 01:26:28 +0800

    HW10 finished

--- 10/main.c
+++ 10/main.c
@@ -3,8 +3,8 @@
 int main(int argc, char const *argv[])
 {
     int seed=Seed();
-    printf("seed=%d",seed);
-    int tau=10,N=1000000,tn=100;
+
+    int tau=10,N=100000,tn=1000;
     //单次行走
     RWThetaToDat(seed,"data_s.dat",tn);
     printf("Complete single walk.\n");
@@ -14,5 +14,9 @@ int main(int argc, char const *argv[])
     CalAvg(seed,"data_avg2.dat",N,tn,2);
     //算自相关
     CalCor(seed,"data_cor.dat",N,tn,tau);
+    //打印种子
+    printf("seed=%d",seed);
     system("pause");
+
+
 }
