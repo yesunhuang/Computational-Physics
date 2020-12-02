479b6368b5dfe0e87981f053dae6f6d283fa0359
YesunHuang <tgbus944812203@hotmail.com>
2020-12-02 11:42:02 +0800

    fix bug

--- 17/main.c
+++ 17/main.c
@@ -3,13 +3,13 @@
 int main(int argc, char const *argv[])
 {
     //文件名
-    char name1[]="chaosX.dat",name2[]="chaosLam.dat";
+    char name1[]="chaosXL.dat",name2[]="chaosLamL.dat";
     char** name=(char **)malloc(2*sizeof(char*));
     name[0]=name1;name[1]=name2;
 
     int M=10000,N=100,n=5000;
     int i;
-    double step=(double)0.5/(double)n,RE=1E-5;
+    double step=(double)4/(double)n,RE=1E-5;
 
     //lambda数组
     double* lambda=(double *)malloc(n*sizeof(double));
@@ -17,10 +17,25 @@ int main(int argc, char const *argv[])
     //初始化lambda数组
     for ( i = 0; i < n; i++)
     {
-        lambda[i]=2.4+(double)i*step;
+        lambda[i]=0+(double)i*step;
     }
     
     chaosToDat(name,lambda,n,M,N,RE);
+
+    //zoomin
+    char name3[]="chaosX.dat",name4[]="chaosLam.dat";
+    name[0]=name3;name[1]=name4;
+
+    step=(double)1/(double)n;
+
+    //初始化lambda数组
+    for ( i = 0; i < n; i++)
+    {
+        lambda[i]=0+(double)i*step;
+    }
+    
+    chaosToDat(name,lambda,n,M,N,RE);
+    
     
 
     return 0;
